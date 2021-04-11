f3d90fda8445d080bd70e9e321a82bd6505b9ead
ZKillou <55600416+ZKillou@users.noreply.github.com>
2021-04-12 00:21:55 +0200

    index.js for 0.1.0 (not tested)

--- index.js
+++ index.js
@@ -1,43 +1,46 @@
 // Ne rien modifier !
 // LaRADIOdugaming module by CreeperGames
-// v0.0.1
+// v0.1.0
 
-const WebSocket = require("ws")
-const wss = new WebSocket.Server({ port: 8082 })
 const RPC = require("discord-rpc")
+const fetch = require("node-fetch")
 
 console.log("Le fichier est en cours d'exécution !")
+const clientId = '767777944096604241';
+const client = new RPC.Client({ transport: 'ipc' });
+let data
 
-wss.on("connection", ws => {
-	console.log("Connecté à votre navigateur.")
-	const clientId = '767777944096604241';
-	const client = new RPC.Client({ transport: 'ipc' });
-
-	client.on('ready', () => {
-		console.log(`Connecté à votre discord, ${client.user.username} !`)
-		ws.on("message", async data => {
-			console.log("Récupération")
-			let object = JSON.parse(data)
-			object = JSON.parse(object)
-			await client.clearActivity()
-			if(object.isPlaying){
-				console.log("Lecture en cours ...")
-				client.setActivity({
-					details: `Ecoute : ${object.songTitle}`,
-					state: 'Dans le salon vocal.',
-					startTimestamp: object.startAt,
-					endTimestamp: object.startAt + (object.songDuration * 1000)
-				}).then(() => console.log("Activité mise à jour !")).catch(error => { console.error(error); ws.close() });
-			}
-		})
-	});
-	
-	client.login({ clientId }).catch(error => {
-		console.error(error)
-		ws.close()
-	})
-	
-	ws.on("close", () => {
-		console.log("Déconnection détéctée")
-	})
+client.login({ clientId }).catch(error => {
+	console.error(error)
+	process.exit()
 })
+
+const main = async () => {
+	await getter()
+	setStaus(data)
+}
+
+const getter = () => {
+	fetch("https://laradiodugaming.glitch.me/np/json")
+		.then(res => res.json())
+		.then(body => data = body)
+	console.log("Les données ont été récupérées avec succès !")
+}
+
+const setStatus = (d) => {
+	let end = d.startAt + (d.songDuration * 1000)
+	client.setActivity({
+		details: `Ecoute : ${d.songTitle}`,
+		state: 'Dans le salon vocal.',
+		startTimestamp: d.startAt,
+		endTimestamp: end
+	}).then(() => {
+		console.log("Activité mise à jour !")
+		setTimeout(async () => main(), end + 10000)
+	}).catch(error => console.error(error))
+}
+
+client.on('ready', () => {
+	console.log(`Connecté à votre discord, ${client.user.username} !`)
+	main()
+});
